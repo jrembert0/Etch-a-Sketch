71c79c275fd91c29c57188edf4ef396225cf1506
jrembert0 <134031855+jrembert0@users.noreply.github.com>
2024-04-19 06:42:56 -0600

    Grid added by user input

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("button");
 
-for(let i = 0; i < 256; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+btn.addEventListener("click", gridSize);
+
+function gridSize() {
+    let text;
+    let grid = prompt("Please enter grid size (max. 100):", "16");
+    if (grid == null || grid == "") {
+        text = "User cancelled the prompt.";
+    }
+    if (grid > 100 || grid < 2){
+        text = "Numbers between 2 and 100 are allowed only";
+    }
+    else{
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+          }
+        for (let i = 1; i <= grid; i++) {
+            let square = document.createElement("div");
+            square.textContent = i;
+            square.classList.add("square");
+            container.appendChild(square);
+        }
+    }
 }
 
 const div = document.querySelectorAll(".square");
-
 div.forEach((square) => {
-    square.addEventListener("onmouseover", function(e) {
+    square.addEventListener("mouseover", function (e) {
         e.target.style.backgroundColor = "blue";
     })
 });
